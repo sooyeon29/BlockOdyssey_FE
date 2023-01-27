31571c079567ea4bff2e5ee853d61504c3e663f4
sooyun <so__oyeonii@naver.com>
2023-01-27 14:36:18 +0900

    [doc] 리액트쿼리설치

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import store from "./redux/config/configStore";
 // import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+// import { ReactQueryDevtools } from "react-query/devtools";
+
 import App from "./App";
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <Provider store={store}>
-  <App />
-  // </Provider>
+  <QueryClientProvider client={queryClient}>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
+  </QueryClientProvider>
 );
