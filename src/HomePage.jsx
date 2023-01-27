aefe261a809ebdf5a7ee126ad2a486220b33e13c
sooyun <so__oyeonii@naver.com>
2023-01-27 15:03:37 +0900

    [doc] 메인홈페이지 컴포넌트 분리/ 파일명 변경

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import StockList from "./components/StockList";
 
-const MainPage = () => {
+const HomePage = () => {
   const [phoneList, setPhoneList] = useState();
 
   useEffect(() => {
@@ -33,45 +34,12 @@ const MainPage = () => {
           <button>조회</button>
         </div>
       </SearchBox>
-      <TotalData>
-        검색된 데이터 :{" "}
-        {phoneList === undefined ? "" : Object.keys(phoneList).length}건
-      </TotalData>
-      <Product>
-        <List>
-          <thead>
-            <Category>
-              <th>상품번호</th>
-              <th>상품명</th>
-              <th>브랜드</th>
-              <th>상품내용</th>
-              <th>가격</th>
-              <th>평점</th>
-              <th>재고</th>
-            </Category>
-          </thead>
-          <tbody>
-            {phoneList?.map((phone) => {
-              return (
-                <Category key={phone.id}>
-                  <td>{phone.id}</td>
-                  <td>{phone.title}</td>
-                  <td>{phone.brand}</td>
-                  <td>{phone.description.substring(0, 40)}...</td>
-                  <td>{phone.price}</td>
-                  <td>{phone.rating}</td>
-                  <td>{phone.stock}</td>
-                </Category>
-              );
-            })}
-          </tbody>
-        </List>
-      </Product>
+      <StockList phoneList={phoneList} />
     </>
   );
 };
 
-export default MainPage;
+export default HomePage;
 
 const SearchBox = styled.div`
   background-color: white;
@@ -114,30 +82,4 @@ const SearchBox = styled.div`
 const Search = styled.div`
   padding: 15px;
   font-weight: bold;
-`;
-
-const TotalData = styled.div`
-  margin: 30px 10px;
-  font-weight: bold;
-`;
-const Product = styled.div`
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 3px 3px 1px rgba(0, 0, 0, 0.1);
-  margin: 20px 10px;
-  padding: 30px 20px;
-`;
-const List = styled.table`
-  margin: auto;
-  width: 100%;
-`;
-const Category = styled.tr`
-  th {
-    border-bottom: 1px solid darkgray;
-  }
-  td {
-    text-align: center;
-    padding: 20px 10px;
-    border-bottom: 1px solid darkgray;
-  }
 `;
---
+++ src/components/StockList.jsx
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+const StockList = ({ phoneList }) => {
+  return (
+    <>
+      <TotalData>
+        검색된 데이터 :{" "}
+        {phoneList === undefined ? "" : Object.keys(phoneList).length}건
+      </TotalData>
+      <Product>
+        <List>
+          <thead>
+            <Category>
+              <th>상품번호</th>
+              <th>상품명</th>
+              <th>브랜드</th>
+              <th>상품내용</th>
+              <th>가격</th>
+              <th>평점</th>
+              <th>재고</th>
+            </Category>
+          </thead>
+          <tbody>
+            {phoneList?.map((phone) => {
+              return (
+                <Category key={phone.id}>
+                  <td>{phone.id}</td>
+                  <td>{phone.title}</td>
+                  <td>{phone.brand}</td>
+                  <td>{phone.description.substring(0, 40)}...</td>
+                  <td>{phone.price}</td>
+                  <td>{phone.rating}</td>
+                  <td>{phone.stock}</td>
+                </Category>
+              );
+            })}
+          </tbody>
+        </List>
+      </Product>
+    </>
+  );
+};
+
+export default StockList;
+
+const TotalData = styled.div`
+  margin: 30px 10px;
+  font-weight: bold;
+`;
+const Product = styled.div`
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 3px 3px 1px rgba(0, 0, 0, 0.1);
+  margin: 20px 10px;
+  padding: 30px 20px;
+`;
+const List = styled.table`
+  margin: auto;
+  width: 100%;
+`;
+const Category = styled.tr`
+  th {
+    border-bottom: 1px solid darkgray;
+  }
+  td {
+    text-align: center;
+    padding: 20px 10px;
+    border-bottom: 1px solid darkgray;
+  }
+`;
