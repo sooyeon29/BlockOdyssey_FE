56a036c6292f0a467f1e98341f5338d0a7cbfced
sooyun <so__oyeonii@naver.com>
2023-01-28 15:06:47 +0900

    [doc] 리액트쿼리사용X

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import store from "./redux/config/configStore";
-// import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "react-query";
+import store from "./redux/config/configStore";
+import { Provider } from "react-redux";
+// import { QueryClient, QueryClientProvider } from "react-query";
 // import { ReactQueryDevtools } from "react-query/devtools";
 
 import App from "./App";
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <QueryClientProvider client={queryClient}>
-    {/* <Provider store={store}> */}
+  // <QueryClientProvider client={queryClient}>
+  <Provider store={store}>
     <App />
-    {/* </Provider> */}
-  </QueryClientProvider>
+  </Provider>
+  //  </QueryClientProvider>
 );
